987fccfce65e3c1b6bf5bb341c293ee5b0b785f9
Yujin1007 <yujin1004k@gmail.com>
2024-06-25 11:22:47 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kist-robot2/Franka/franka_cabinet
+# Build directory: /home/kist-robot2/Franka/franka_cabinet/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
